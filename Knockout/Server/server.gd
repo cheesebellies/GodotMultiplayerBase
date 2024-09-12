7396bd2b31df57b5418e96f253b8651723299027
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2024-09-12 11:07:42 -0400

    Update server.gd

--- Knockout/Server/server.gd
+++ Knockout/Server/server.gd
@@ -36,6 +36,39 @@ func _ready() -> void:
 		multiplayer.peer_packet.connect(_endpoint_packet_received)
 		debuge("Connected to server")
 
+#Bit processing
+
+func unpack_positional(packet: PackedByteArray):
+	pass
+
+func pack_positional(node: Node3D):
+	#***********************************************************
+	# Note that a normal float is 8 bytes, but for both Vector3
+	# and Quaternion, they are reduced to 4 bytes for speed.
+	# With PackedByteArray, 'double' is a normal float and
+	# 'float' is the 32 bit version
+	#
+	# Position: Vector3: 3 floats (4, 4, 4 bytes)
+	# Velocity: Vector3: 3 floats (4, 4, 4 bytes)
+	# Rotation: Quaternion: 4 floats (4, 4, 4, 4 bytes)
+	#***********************************************************
+	var pos = node.position
+	var vel = node.velocity
+	var rot = node.quaternion
+	var packet = PackedByteArray()
+	packet.resize(40)
+	packet.encode_float(0,pos.x)
+	packet.encode_float(4,pos.y)
+	packet.encode_float(8,pos.z)
+	packet.encode_float(12,vel.x)
+	packet.encode_float(16,vel.y)
+	packet.encode_float(20,vel.z)
+	packet.encode_float(24,rot.w)
+	packet.encode_float(28,rot.x)
+	packet.encode_float(32,rot.y)
+	packet.encode_float(36,rot.z)
+	return packet
+
 #Processing functions
 
 func ping_server():
