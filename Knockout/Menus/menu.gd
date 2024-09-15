4dad354d533ce523b9e61c8f3f44da4e100d8d42
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2024-09-15 19:58:17 -0400

    sdfg

--- Knockout/Menus/menu.gd
+++ Knockout/Menus/menu.gd
@@ -48,3 +48,7 @@ func _physics_process(_delta):
 	$Camera3D.position = Vector3(10.0*cos(deg_to_rad(float(i)*0.5)),10,10.0*sin(deg_to_rad(float(i)*0.5)))
 	$Camera3D.look_at(Vector3(0,5,0))
 	tick += 1
+
+
+func _on_quit_pressed():
+	get_tree().quit()
